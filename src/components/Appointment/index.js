c126f822dcb02c0d72a82ffd4be3465ca766782c
Paulo <phmelomorais@gmail.com>
2019-09-14 17:52:27 -0300

    feature/Dashboard

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Left, Avatar, Info, Name, Time } from './styles';
+
+export default function Appointment() {
+  return (
+    <Container>
+      <Left>
+        <Avatar source={{ uri: 'http://api.adorable.io/avatar/50/test.png' }} />
+        <Info>
+          <Name>PH MELO</Name>
+          <Time>em 3 horas</Time>
+        </Info>
+      </Left>
+      <TouchableOpacity onPress={() => {}}>
+        <Icon name="event-busy" size={20} color="#f64c75" />
+      </TouchableOpacity>
+    </Container>
+  );
+}
